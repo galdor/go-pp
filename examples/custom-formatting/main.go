780603c3123e9ca4b3a6ac40e35413b20b7e6f8b
Nicolas Martyanoff <nicolas@n16f.net>
2024-11-09 18:23:54 +0100

    improve examples

---
+++ examples/custom-formatting/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/galdor/go-pp"
+)
+
+type Id struct {
+	Type  string
+	Value int
+}
+
+func (id Id) String() string {
+	return fmt.Sprintf("%s:%d", id.Type, id.Value)
+}
+
+func FormatValue(v reflect.Value) any {
+	if id, ok := v.Interface().(Id); ok {
+		return pp.RawString(id.String())
+	}
+
+	return pp.FormatValue(v)
+}
+
+func main() {
+	pp.Print(Id{"user", 42}, "default format")
+
+	pp.DefaultPrinter.SetFormatValueFunc(FormatValue)
+	pp.Print(Id{"user", 42}, "custom format")
+}
