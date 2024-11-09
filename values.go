3e98142858d3b4a52347b81fbc2907a8ba639639
Nicolas Martyanoff <nicolas@n16f.net>
2024-11-09 15:45:27 +0100

    add regexp formatting

--- values.go
+++ values.go
@@ -2,25 +2,33 @@ package pp
 
 import (
 	"reflect"
+	"regexp"
 	"time"
 )
 
 func ValueString(v reflect.Value) string {
 	switch vv := v.Interface().(type) {
-	case time.Time:
-		return TimeValueString(vv)
+	case regexp.Regexp:
+		return RegexpValueString(&vv)
 
 	case time.Duration:
 		return DurationValueString(vv)
+
+	case time.Time:
+		return TimeValueString(vv)
 	}
 
 	return ""
 }
 
-func TimeValueString(t time.Time) string {
-	return t.Format(time.RFC3339Nano)
+func RegexpValueString(re *regexp.Regexp) string {
+	return "/" + re.String() + "/"
 }
 
 func DurationValueString(d time.Duration) string {
 	return d.String()
+}
+
+func TimeValueString(t time.Time) string {
+	return t.Format(time.RFC3339Nano)
 }
